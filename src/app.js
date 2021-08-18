faf1ae16fd98fa0f6621767e0c883c8edd4622f0
weinge <wein.geoffrey@gmail.com>
2021-08-18 01:29:08 -0500

    adjusted file structure for handlebars, set static route, had to revert map code

--- src/app.js
+++ src/app.js
@@ -9,20 +9,32 @@ require('dotenv').config();
 
 require('./auth/passport');
 
-require('./models/user');
-
-const middlewares = require('./middlewares');
+// Get API information
 const api = require('./routes');
 
+// Set up Express App
 const app = express();
+app.use(express.static('public'));
+
+//Loads the handlebars module
+var exphbs = require('express-handlebars');
+
+// Use cookieParser
 app.use(cookieParser());
 app.use(morgan('dev'));
+
+// Set up CORS
 app.use(cors());
 app.use(express.json());
 
+// Use defined API
 app.use('/api/v1', api);
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+	res.render('main', { layout: false });
+});
 
 module.exports = app;
