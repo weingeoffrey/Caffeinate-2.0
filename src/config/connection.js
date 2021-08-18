2ec5f2d5186507d589a607737ed556a568666e94
weinge <wein.geoffrey@gmail.com>
2021-08-17 20:56:51 -0500

    Fixed package and API routes

--- src/config/connection.js
+++ src/config/connection.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
 	process.env.DB_NAME,
 	process.env.DB_USER,
-	process.env.DB_PASS,
+	process.env.DB_PW,
 	{
 		host: 'localhost',
 		dialect: 'mysql',
